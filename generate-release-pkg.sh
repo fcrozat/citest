443b5c48f3b4d63491821e199c4744566ea09914
Stephan Kulow <coolo@suse.de>
2019-02-14 21:25:35 +0100

    Export first

--- generate-release-pkg.sh
+++ generate-release-pkg.sh
@@ -4,10 +4,10 @@ set -x
 
 set
 
+export PYTHONPATH=/usr/share/openSUSE-release-tools
+
 python ./report-status.py -A $STAGING_API -p $STAGING_PROJECT -r standard -s pending
 exit 1
-
-export PYTHONPATH=/usr/share/openSUSE-release-tools
 
 if /usr/bin/osrt-pkglistgen -A $STAGING_API update_and_solve --staging $STAGING_PROJECT --only-release-packages --force; then
    python ./report-status.py -A $STAGING_API -p $STAGING_PROJECT -r standard -s pending
