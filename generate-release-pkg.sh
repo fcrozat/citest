b5df6b4a9941c82461ba307007e1ad0261509be1
Stephan Kulow <coolo@suse.de>
2019-02-14 21:34:02 +0100

    Remove debug again

--- generate-release-pkg.sh
+++ generate-release-pkg.sh
@@ -2,12 +2,7 @@
 
 set -x
 
-set
-
 export PYTHONPATH=/usr/share/openSUSE-release-tools
-
-python ./report-status.py -A $STAGING_API -p $STAGING_PROJECT -r standard -s pending
-exit 1
 
 if /usr/bin/osrt-pkglistgen -A $STAGING_API update_and_solve --staging $STAGING_PROJECT --only-release-packages --force; then
    python ./report-status.py -A $STAGING_API -p $STAGING_PROJECT -r standard -s pending
